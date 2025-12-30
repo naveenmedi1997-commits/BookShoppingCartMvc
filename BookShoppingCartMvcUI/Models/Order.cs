eb83f99d76e87f3fc3468f984ca60188fe28aa43
Naveen M <naveenmedi1997@gmail.com>
2025-12-30 21:20:06 +0530

    All models clases are added

---
+++ BookShoppingCartMvcUI/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        public DateTime CreateDate { get; init; } = DateTime.UtcNow;
+        [Required]
+        public int OrderStatusId { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderDetail> OrderDetail { get; set; }
+    }
+}
---
+++ BookShoppingCartMvcUI/Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCartMvcUI.Models
+{
+    public class OrderStatus
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(20)]
+
+        public string? StatusName { get; set; }
+    }
+}
